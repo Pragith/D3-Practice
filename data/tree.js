8d92018365e9c9c4e156fd9ed4240688a3ed9874
Pragith <git@pragith.net>
2014-01-15 22:13:03 +0530

    2 more videos remaining

---
+++ data/tree.js
@@ -0,0 +1,27 @@
+{
+	"name": "Max",
+	"value": 100,
+	"children":[
+		{
+			"name": "Sylvia",
+			"value": 75,
+			"children":[
+				{ "name" : "Craig","value": 50 },
+				{ "name" : "Robin","value": 50 },
+				{ "name" : "Anna","value": 50 }
+			]
+		},
+		{
+			"name": "David",
+			"value": 75,
+			"children":[
+				{ "name" : "Jeff","value": 50 },
+				{ "name" : "Buffy","value": 50 }
+			]
+		},
+		{
+			"name": "Mr. X",
+			"value": 75
+		}
+	]
+}
